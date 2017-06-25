7453c54a4cfca4c712fc6cf3bf2725c90aac15e6
J053Fabi0 <arguellojf1@gmail.com>
2017-06-25 13:29:48 -0600

    First commit

---
+++ AppAsset/RootPageViewController.swift
@@ -0,0 +1,69 @@
+//
+//  RootPageViewController.swift
+//  AppAsset
+//
+//  Created by Jose Fabio_ on 23/06/17.
+//  Copyright © 2017 com.yoyomero.josefabio. All rights reserved.
+//
+
+
+
+import UIKit
+
+class RootPageViewController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+    
+    lazy var viewControllerList: [UIViewController] = {
+        
+        let sb = UIStoryboard(name: "Main", bundle: nil) //Story Board
+        
+        let VCrs: [UIViewController] = [ //View Controllers
+            sb.instantiateViewController(withIdentifier: "Fluttershy"),
+            sb.instantiateViewController(withIdentifier: "PinkiePie"),
+            sb.instantiateViewController(withIdentifier: "AppleJack")]
+        
+        return VCrs
+    }()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.dataSource = self
+        
+        if let firstViewController: UIViewController = viewControllerList.first{
+            self.setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let vcIndex = viewControllerList.index(of: viewController) else {return nil}
+        
+        let previousIndex = vcIndex - 1
+        
+        guard previousIndex >= 0 else {return viewControllerList[viewControllerList.count - 1]}
+        
+        guard viewControllerList.count > previousIndex else {return nil}
+        
+        return viewControllerList[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let vcIndex = viewControllerList.index(of: viewController) else {return nil}
+        
+        let nextIndex = vcIndex + 1
+        
+        guard viewControllerList.count != nextIndex else {return viewControllerList[0]}
+        
+        guard viewControllerList.count > nextIndex else {return nil}
+        
+        return viewControllerList[nextIndex]
+    }
+    
+
+}
